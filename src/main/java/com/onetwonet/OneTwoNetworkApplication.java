9cfaee999e710786c1230b5203cc3083180beb06
Vlad Radchenko <vladyslav.radchenko@gmail.com>
2023-01-17 13:10:01 +0000

    css style added

--- src/main/java/com/onetwonet/OneTwoNetworkApplication.java
+++ src/main/java/com/onetwonet/OneTwoNetworkApplication.java
@@ -2,10 +2,6 @@ package com.onetwonet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages= "com")
 public class OneTwoNetworkApplication {
--- src/main/java/com/onetwonet/contoller/FrontController.java
+++ src/main/java/com/onetwonet/contoller/FrontController.java
@@ -5,26 +5,37 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
-@RequestMapping("/")
 @RequiredArgsConstructor
 public class FrontController {
     private final BetService betService;
 
-    @GetMapping()
-    public ModelAndView mainBetPage(HttpServletRequest request) {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public void mainBetPage(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.sendRedirect(request.getContextPath() + "bet/list");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/bet/list")
+    public ModelAndView betPage(HttpServletRequest request) {
 
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("bets", betService.findAll());
         return modelAndView;
     }
 
-    @GetMapping("/game/{game}")
+    @GetMapping("/bet/game/{game}")
     public ModelAndView filterGame(@PathVariable("game") String game) {
 
         ModelAndView modelAndView = new ModelAndView("game");
@@ -33,7 +44,7 @@ public class FrontController {
         return modelAndView;
     }
 
-    @GetMapping("/clientid/{clientid}")
+    @GetMapping("/bet/client/{clientid}")
     public ModelAndView filterClientId(@PathVariable("clientid") String clientId) {
 
         ModelAndView modelAndView = new ModelAndView("clientid");
@@ -41,7 +52,7 @@ public class FrontController {
         return modelAndView;
     }
 
-    @GetMapping("/date/{date}")
+    @GetMapping("/bet/date/{date}")
     public ModelAndView filterDate(@PathVariable("date") String date) {
 
         ModelAndView modelAndView = new ModelAndView("date");
